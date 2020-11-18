ca7780086cf042446ffe3ed35def56a223f6c19e
BrissaLópez <brissa.lopez92@gmail.com>
2020-11-18 16:24:58 -0300

    cuarto avance: escoger palabras funciona y está ordenado

--- Algoritmo.php
+++ Algoritmo.php
@@ -38,7 +38,8 @@ class Algoritmo{
           $this->grupo_cuatro = ["boca"=>54, "boda"=>55, "codo"=>56, "condado"=>57, "contado"=>58, "domar"=>59, "saldar"=>60, "saltado"=>61,
               "saltar"=>62, "seda"=>63, "soldado"=>64, "tienda"=>65, "tiendo"=>66, "tienta"=>67, "tiento"=>68, "tomar"=>69, "zeta"=>70];
           
-          $this->grupo_cinco = [];
+          $this->grupo_cinco = ["cama"=>71, "cana"=>72, "casa"=>73, "caucho"=>74, "coma"=>75, "corro"=>76, "gama"=>77, "gana"=>78, 
+              "gasa"=>79, "gaucho"=>80, "goloso"=>81, "goma"=>82, "gorro"=>83, "guita"=>84, "quita"=>85, "toca"=>86, "toga"=>87, "vaca"=>88];
           
           $this->listado = ["grupo1", "grupo2", "grupo2", "grupo4", "grupo5"];
            
@@ -55,9 +56,21 @@ class Algoritmo{
         for($ciclo = 0; $ciclo <=2; $ciclo++){
                 
             if($ciclo == 1){
+                
+                $this->seleccionaGrupo();
+            }
+            
+            if($ciclo == 2){ //si ciclo == 2, se hace el oído izquierdo
+                
+                $this->seleccionaGrupo();
                     
-                    
-                $grupo = array_rand($this->listado);
+            }
+        }
+    }
+    
+    public function seleccionaGrupo(){
+        
+        $grupo = array_rand($this->listado);
                     
                 $grupoString = $this->listado[$grupo];
                     
@@ -68,71 +81,37 @@ class Algoritmo{
                     
                 if(strcmp($grupoString, "grupo1")== 0){
                     echo " = grupo1"."<br>";
-                    $this->ladoDerecho($grupo);
+                    
+                    $palabrasSeleccionadas = array_rand($this->grupo_uno, 10);
+                
+                    $this->seleccionaPalabra($palabrasSeleccionadas);
+                    
                 }else if (strcmp($grupoString, "grupo2")== 0){
                     echo " = grupo2"."<br>";
+                    $palabrasSeleccionadas = array_rand($this->grupo_dos, 10);
+                    $this->seleccionaPalabra($palabrasSeleccionadas);
+                    
                 }else if (strcmp($grupoString, "grupo3")== 0){
                     echo " = grupo3"."<br>";
+                    
+                    $palabrasSeleccionadas = array_rand($this->grupo_tres, 10);
+                    $this->seleccionaPalabra($palabrasSeleccionadas);
+                    
                 }else if (strcmp($grupoString, "grupo4")== 0){
                     echo " = grupo4"."<br>";
+                    
+                    $palabrasSeleccionadas = array_rand($this->grupo_cuatro, 10);
+                    $this->seleccionaPalabra($palabrasSeleccionadas);
+                    
                 }else if (strcmp($grupoString, "grupo5")== 0){
                     echo " = grupo5"."<br>";
+                    
+                    $palabrasSeleccionadas = array_rand($this->grupo_cinco, 10);
+                    $this->seleccionaPalabra($palabrasSeleccionadas);
+                    
                 }else {
                     echo " Ha habido un error al escoger un grupo";
                 }
-                   
-            }
-            
-            if($ciclo == 2){ //si ciclo == 2, se hace el oído izquierdo
-                    
-                $grupo = array_rand($this->listado);
-                    
-                $grupoString = $this->listado[$grupo];
-                    
-                echo "<br>      Se ha escogido: ".$grupoString;
-                    
-                    //dependiendo del grupo escogido, se comenzará con la selección de palabras del grupo
-                    
-                if(strcmp($grupoString, "grupo1")== 0){
-                    echo " = grupo1"."<br>";
-                }else if (strcmp($grupoString, "grupo2")== 0){
-                    echo " = grupo2"."<br>";
-                }else if (strcmp($grupoString, "grupo3")== 0){
-                    echo " = grupo3"."<br>";
-                }else if (strcmp($grupoString, "grupo4")== 0){
-                    echo " = grupo4"."<br>";
-                }else if (strcmp($grupoString, "grupo5")== 0){
-                    echo " = grupo5"."<br>";
-                }else {
-                    echo " Ha habido un error al escoger un grupo";
-                }
-            }
-        }
-    }
-    
-    public function ladoDerecho($grupoEscogido){
-        
-        switch ($grupoEscogido){
-            
-            case "grupo1":
-                //var_export($this->grupo_uno_derecha);
-                $palabrasSeleccionadas = array_rand($this->grupo_uno, 10);
-                
-                $this->seleccionaPalabra($palabrasSeleccionadas);
-                
-            case "grupo2":
-                $palabrasSeleccionadas = array_rand($this->grupo_dos);
-                $this->seleccionaPalabra($arrayPalabrasSeleccionadas);
-                    
-    
-                
-                
-            default:
-                echo " aun no se ha creado el grupo.";
-                
-        }
-        
-        
         
     }
     
